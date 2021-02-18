1d2463f9725ce0b10c989bbc5d121cc03031fe79
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-18 19:18:10 +0100

    added attributes name inside inpute to have the key inside the request variable

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,8 +10,8 @@
         <h1>Write a New Post</h1>
         <form action="{{ route('posts.store')}}" method="post">
             @csrf
-            <input type="text" placeholder="Write here the title of your post">
-            <input type="text" placeholder="Write here the body of your post">
+            <input type="text" name="title" placeholder="Write here the title of your post">
+            <input type="text" name="body" placeholder="Write here the body of your post">
             <button type="submit">Publish</button>
         </form>
     </body>
